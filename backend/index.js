686459b28d1c91445c2b03fbc7aa4b3003769f77
PuelloJ <josepuello112000@gmail.com>
2025-09-08 18:36:16 -0500

    Add initial server setup and books route

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer')
+const path = require('path');
+
+// Initialization
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'))
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname))
+    }
+})
+
+//  File upload 
+app.use(multer({ storage }).single('image')) // name of the input file
+
+// Parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(express.json());
+
+// Routes
+app.use('/api/books', require('./routes/books'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
